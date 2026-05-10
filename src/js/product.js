31be79b1f7e8e2215de2f70f546bbeabdfd0fc1a
Sebasosax <Sosa.sc@outlook.com>
2026-05-09 22:44:19 -0600

    fix add cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,18 +1,27 @@
-import { setLocalStorage } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import { setLocalStorage, getLocalStorage } from './utils.mjs';
+import ProductData from './ProductData.mjs';
 
-const dataSource = new ProductData("tents");
+const dataSource = new ProductData('tents');
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // Get existing cart or start empty array
+  let cart = getLocalStorage('so-cart') || [];
+
+  // Add new product
+  cart.push(product);
+
+  // Save updated cart
+  setLocalStorage('so-cart', cart);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
+
   addProductToCart(product);
 }
 
 // add listener to Add to Cart button
 document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+  .getElementById('addToCart')
+  .addEventListener('click', addToCartHandler);
